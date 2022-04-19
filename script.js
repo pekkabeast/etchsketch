a53cdebe6df88bfabc1ac6f54ba0c77e552bc4b4
Victor <vctrchn@hotmail.ca>
2022-04-19 15:58:48 -0400

    added click events

--- script.js
+++ script.js
@@ -3,20 +3,30 @@
 const mainDiv = document.querySelector('div');
 
 
- for (let i = 0; i < 16; i++){
+for (let i = 0; i < 16; i++) {
 
     let div = document.createElement('div');
-    div.setAttribute('id',`${i+1}-0`);
+    div.setAttribute('id', `${i + 1}-0`);
     div.classList.add('flex')
-    for (let j = 0; j < 16; j++){
+    for (let j = 0; j < 16; j++) {
         let innerDiv = document.createElement('div');
-        innerDiv.setAttribute('id',`${i+1}-${j+1}`);
+        innerDiv.setAttribute('id', `${i + 1}-${j + 1}`);
         innerDiv.classList.add('square');
-        
-        div.appendChild(innerDiv);
+        mainDiv.addEventListener('mousedown', function () {
+            innerDiv.addEventListener('mouseover',change)
+        })
+        mainDiv.addEventListener('mouseup', function(){
+            innerDiv.removeEventListener('mouseover',change)
+        })
 
+        div.appendChild(innerDiv);
     }
+
     mainDiv.appendChild(div);
 
- }
+}
 
+
+function change(){
+    this.classList.add('change');
+}
